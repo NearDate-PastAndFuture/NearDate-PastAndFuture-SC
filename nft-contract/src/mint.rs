aa57a4150fe4ebacc0d5dfbbfcea1b5b883bda52
HDTung <haduytung93@gmaiil.com>
2022-08-18 12:02:01 +0700

    add check valid date and price mint condition

--- nft-contract/src/mint.rs
+++ nft-contract/src/mint.rs
@@ -1,7 +1,47 @@
 use crate::*;
 
+pub(crate) fn is_valid_date(dateString: TokenId) -> bool //yyyymmdd
+{
+    let day = &dateString[6..8].parse::<i32>().unwrap();
+    let month = &dateString[4..6].parse::<i32>().unwrap();
+    let year = &dateString[0..4].parse::<i32>().unwrap();
+
+    // Check the ranges of month and year
+    if(*year < 1000 || *year > 3000 || *month == 0 || *month > 12){
+        return false;
+    }
+
+    let mut monthLength = vec![ 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 ];
+
+    // Adjust for leap years
+    if(*year % 400 == 0 || (*year % 100 != 0 && *year % 4 == 0)){
+        monthLength[1] = 29;
+    }
+
+    // Check the range of the day
+    if( *day <= 0 || *day > monthLength[(*month-1) as usize])
+    {
+        return false;
+    }
+
+    // Check limit NFT can be mint
+    //19330605 -> 21400101
+    if(*year < 1933 || *year > 2140)
+    {
+        return false;
+    }else if(*year == 1933 && (*month < 6 || (*month == 6 && *day < 5)) )
+    {
+        return false;
+    }else if(*year == 2140 && (*month > 1 || *day > 1) )
+    {
+        return false;
+    }
+    return true;
+}
+
 #[near_bindgen]
 impl Contract {
+
     #[payable]
     pub fn nft_mint(
         &mut self,
@@ -11,17 +51,37 @@ impl Contract {
         message_url: String,
         //we add an optional parameter for perpetual royalties
         perpetual_royalties: Option<HashMap<AccountId, u32>>,
+        price: Option<U128>,
     ) {
         //TODO assert() format token_id
+        let is_valid = is_valid_date(token_id.clone());
+        assert!(is_valid, "not a validate date");
 
         //check if owner then free, otherwise if user then charge fee 
         //check max_free_nft_number
         let account_id = env::predecessor_account_id();
-        if account_id == self.owner_id {
-            // free mint
+
+        let mut mint_fee :U128 = U128(0);
+        if(self.token_metadata_by_id.len() > MAX_FREE_NFT)
+        {
+            if let Some(mint_price) = price
+            {
+                mint_fee = mint_price ;
+            }
+            else{
+                mint_fee = U128(1_000_000_000_000_000_000_000_000); // 1 $NEAR as yoctoNEAR
+            }
         }
 
-        let deposit = env::attached_deposit();
+        if account_id == self.owner_id {
+            // free mint
+            mint_fee = U128(0);
+        }
+
+        // check 
+        let attached_deposit = env::attached_deposit();
+
+        assert!(attached_deposit > mint_fee.0, "Attached deposit must be greater than mint price: {:?}", mint_fee);
 
         //measure the initial storage being used on the contract
         let initial_storage_usage = env::storage_usage();
@@ -87,7 +147,7 @@ impl Contract {
         env::log_str(&nft_mint_log.to_string());
 
         //calculate the required storage which was the used - initial
-        let required_storage_in_bytes = env::storage_usage() - initial_storage_usage;
+        let required_storage_in_bytes = env::storage_usage() - initial_storage_usage ;
 
         //refund any excess storage if the user attached too much. Panic if they didn't attach enough to cover the required.
         refund_deposit(required_storage_in_bytes);
